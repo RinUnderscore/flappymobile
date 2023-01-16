8a8499d0922b954a6a933dc6d7770a2e7ea53b12
Ethan Y <88380314+RinUnderscore@users.noreply.github.com>
2023-01-16 18:52:01 -0500

    v1.3.0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.locals import *
+from pygame.freetype import *
 import time
 import random
 
@@ -32,12 +33,24 @@ c_pass = False
 pg.init()
 screen = pg.display.set_mode(screensize)
 pg.display.set_caption("FlappyMobile Clone")
+pg.display.set_icon(pg.image.load('bird.png'))
+
+pg.font.init()
+font = pg.freetype.Font("FreeSansBold.ttf", 64)
+
+# Graphics Import
+bird = pg.image.load('bird.png')
+bottom = pg.image.load('bottom.png')
+bg = pg.image.load('images.png')
+bg = pg.transform.scale(bg,(440,760))
+
 run = True
 
 while run:
 
     screen.fill((255,255,255))
-    pg.draw.rect(screen,(0,0,0), pg.Rect(player_data))
+    screen.blit(bg, (0,0))
+    screen.blit(bird, (player_data))
 
     time.sleep(0.01)
     if player_y_accel > 8:
@@ -50,20 +63,20 @@ while run:
     player_data = (player_x, player_y + player_y_change, 50, 50)
 
     # Pipe Code
-    pg.draw.rect(screen,(0,0,0), pg.Rect((pipe_a_x,pipe_a_y,80,700)))
-    pg.draw.rect(screen,(0,0,0), pg.Rect((pipe_a_x,pipe_a_y + 900, 80,700)))
+    pg.draw.rect(screen,(34,139,34), pg.Rect((pipe_a_x,pipe_a_y,80,700)))
+    pg.draw.rect(screen,(34,139,34), pg.Rect((pipe_a_x,pipe_a_y + 900, 80,700)))
     
     pipe_a_x -= 3
 
     # Pipe 2 Code
-    pg.draw.rect(screen,(0,0,0), pg.Rect((pipe_b_x,pipe_b_y,80,700)))
-    pg.draw.rect(screen,(0,0,0), pg.Rect((pipe_b_x,pipe_b_y + 900, 80,700)))
+    pg.draw.rect(screen,(34,139,34), pg.Rect((pipe_b_x,pipe_b_y,80,700)))
+    pg.draw.rect(screen,(34,139,34), pg.Rect((pipe_b_x,pipe_b_y + 900, 80,700)))
     
     pipe_b_x -= 3
 
     # Pipe 3 Code
-    pg.draw.rect(screen,(0,0,0), pg.Rect((pipe_c_x,pipe_c_y,80,700)))
-    pg.draw.rect(screen,(0,0,0), pg.Rect((pipe_c_x,pipe_c_y + 900, 80,700)))
+    pg.draw.rect(screen,(34,139,34), pg.Rect((pipe_c_x,pipe_c_y,80,700)))
+    pg.draw.rect(screen,(34,139,34), pg.Rect((pipe_c_x,pipe_c_y + 900, 80,700)))
     
     pipe_c_x -= 3
 
@@ -85,8 +98,6 @@ while run:
     if c_pass == True and pipe_c_x < (80-25-60):
         c_pass = False
 
-    print(score)
-
     # Pipe Repeat System
     if a_pass == False and score%1==0 and score != 0 and not score%2==0 and not score%3==0:
         pipe_a_x = pipe_c_x + 300
@@ -94,6 +105,13 @@ while run:
         pipe_b_x = pipe_a_x + 300
     if c_pass == False and score%3==0 and score != 0:
         pipe_c_x = pipe_b_x + 300
+
+    # Create Score Render
+    text_surface, rect = font.render(f"{score}", (0,0,0))
+    if score < 10:
+        screen.blit(text_surface, (210,100))
+    elif score >= 10:
+        screen.blit(text_surface, (190,100))
 
     pg.display.update()
 
